ac6c8f365153910d97d59bfcc06e426a9050ccbf
unknown <diogocabete@gmail.com>
2019-05-04 21:47:32 -0700

    Added Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h2>Todo List in Vue.js</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style>
+img {
+    height: 100px;
+}
+</style>

4b4319dcc6d5b53d4f3846365cf675899b79f6c5
unknown <diogocabete@gmail.com>
2019-05-04 21:47:03 -0700

    Added AddTodo component

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="addTodoContainer">
+        <input type="text" v-model="newTodo" placeholder="Add Todo" @keyup.enter="addTodo">
+        <button @click="addTodo" class="addBtn">Add Todo</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddTodo",
+    props: ["todos"],
+    data() {
+        return {
+            newTodo: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            if(this.newTodo === ''){
+                alert("Todo can't be empty")
+            } else {
+                this.todos.push({title: this.newTodo, completed: false})
+                this.newTodo = ''
+            }
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+input[type="text"] {
+    padding: 8px 2px;
+    margin-right: 1em;
+    margin-bottom: 1em;
+    font-size: 18px;
+}
+
+button {
+    color: white;
+    padding: 10px;
+    background: #007bff;
+    border: none;
+    border-radius: 5px;
+    outline:none;
+    cursor: pointer;
+    font-size: 18px;
+}
+</style>
